83f26da224df9a332425f7b4b75ebeeec3de0742
Janita Chalam <janitachalam@gmail.com>
2016-06-23 11:53:48 -0700

    before merge: :

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+
+var url = 'mongodb://localhost:27017/my_database_name';
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
+
+// CONNECTING VIA SOCKET
 
 io.on('connection', function(socket){
 	console.log('a user connected');
@@ -20,4 +26,21 @@ io.on('connection', function(socket){
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
+});
+
+// DB OPERATIONS
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
 });
