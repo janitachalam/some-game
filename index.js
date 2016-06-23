03ac423054f073bff2c184bfb6804d471eb09602
Madison Burke <mburke16@amherst.edu>
2016-06-23 11:26:50 -0500

    completely socket based

--- index.js
+++ index.js
@@ -2,22 +2,48 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+
+///////////// how to reach index page////////////////
+/////////////////////////////////////////////////////
+app.get('/', function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 
+
+/////////////////////////////////////////////////////
+/// protocol for connecting to chat rooms ///////////
+/////////////////////////////////////////////////////
 io.on('connection', function(socket){
-	console.log('a user connected');
+/// room to connect to
+/// stage or comment channel 
+	console.log('bingo');
+
+/////////////////////////////////////////////////////
+	socket.on('make room', function(name){
+		/// stuff happens here
+		var player = 'player1';
+		socket.emit('connected to ' + name + ' as '+player);
+	});
+/////////////////////////////////////////////////////
+	socket.on('join as debater', function(name){
+		/// stuff stuff
+		var player = 'player2';
+		socket.emit('connected to '+ name + ' as '+player);
+	});
+/////////////////////////////////////////////////////
+	socket.on('join as comentator', function(name){
+		/// stuff stuff
+		socket.emit('connected to '+ name + ' as commentator');
+	});
+/////////////////////////////////////////////////////
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
-	io.on('connection', function(socket){
-		socket.on('chat message', function(msg){
-			io.emit('chat message', msg);
-		});
-	});
 });
 
-http.listen(3000, function(){
+
+http.listen(3000,function(){
 	console.log('listening on *:3000');
 });
