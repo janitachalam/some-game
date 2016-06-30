cc989b7cb153a925c8d7a821f6009ed69800cad8
Janita Chalam <janitachalam@gmail.com>
2016-06-30 11:28:24 -0700

    create room socket join

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongodb = require('mongodb');
+var mongoose = require('mongoose');
 var MongoClient = mongodb.MongoClient;
 
-var url = 'mongodb://localhost:27017/my_database_name';
+// connect to DB
+mongoose.connect('mongodb://localhost:27017/debates_db');
+
+
+// create chat schema
+var Schema = mongoose.Schema;
+var chatSchema = new Schema({
+	ID: Number,
+	topic: String,
+	debaters: {debater_1: Number, debater_2: Number},
+	debate_log_1: String,
+	debate_log_2: String,
+	comments: String
+});
+
+var Chat = mongoose.model('Chat', chatSchema);
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -17,19 +33,35 @@ app.get('/chat', function(req, res) {
 /////////////////////////////////////////////////////
 /// protocol for connecting to chat rooms ///////////
 /////////////////////////////////////////////////////
-io.on('connect', function(socket){
-	console.log('received connection');
-/////////////////////////////////////////////////////
-	socket.on('create_room', function(name){
-		// get last room id from DB and increment
-		console.log('received room connection');
-		room_id = 1;
-		var nsp = io.of('/'+room_id);
+io.on('connect', function(socket) {
 
-		// generate new chat room view
-		socket.emit('room_id', room_id);
-		// add chat room to DB
+/////////////////////////////////////////////////////
+
+	socket.on('create_room', function(name) {
+		// Chat.remove({}, function(err) {
+		// 	//console.log(err);
+		// });
+
+		Chat.findOne({}).sort('-ID').exec(function(err, res) { //find max existing ID
+			id = res.ID;
+			if (id == null) id = 1;
+			else id+=1;
+			socket.join(id); // assign socket to new chat room
+			console.log(id);
+			var info = {
+				ID: id, 
+				topic: undefined, 
+				debaters: undefined, 
+				debate_log_1: undefined, 
+				debate_log_2: undefined,
+				comments: undefined 
+				};
+			chat = new Chat(info);
+			chat.save();
+		});
+
 	});
+
 /////////////////////////////////////////////////////
 	socket.on('join_as_debater', function(name){
 		/// stuff stuff
@@ -41,10 +73,7 @@ io.on('connect', function(socket){
 		/// stuff stuff
 		socket.emit('connected to '+ name + ' as commentator');
 	});
-/////////////////////////////////////////////////////
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
+
 });
 
 
@@ -52,9 +81,12 @@ http.listen(3000,function(){
 	console.log('listening on *:3000');
 });
 
+
 // DB OPERATIONS
 
-// Use connect method to connect to the Server
+
+// // Use connect method to connect to the Server
+
 // MongoClient.connect(url, function (err, db) {
 //   if (err) {
 //     console.log('Unable to connect to the mongoDB server. Error:', err);
@@ -63,7 +95,7 @@ http.listen(3000,function(){
 //     console.log('Connection established to', url);
 
 //     var chat_rooms = db.collection('chat_rooms');
-
+		
 //     //Close connection
 //     db.close();
 //   }
